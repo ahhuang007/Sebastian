24bde1f5d21e9ac45561ab628b71dae2e3ec922f
ahhuang007 <ahhuang007@gmail.com>
2021-10-24 14:51:17 -0400

    added incentives
    sebastian still staying still like a lazy bum, so I added the condition that if he doesn't move far enough in enough time, the episode ends

--- gym-seb/gym_seb/envs/seb_env.py
+++ gym-seb/gym_seb/envs/seb_env.py
@@ -104,6 +104,8 @@ class SebEnv(gym.Env):
     if nep[1] > 0.5:
       done = True
       print("y position is over 0.5 at timestep " + str(self.episode_number))
+    elif nep[0] < 0.5 and self.episode_number > 5000:
+        print("hasn't gone far enough")
     elif self.episode_number > self.max_timesteps:
       done = True
       print("max timesteps reached at timestep " + str(self.episode_number))
