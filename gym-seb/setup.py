68ac77bcca1b20f877697ee62be8d889e25ffed6
ahhuang007 <ahhuang007@gmail.com>
2021-12-12 01:40:17 -0500

    updated env version
    felt like updating gym to 0.0.5

--- gym-seb/setup.py
+++ gym-seb/setup.py
@@ -8,7 +8,7 @@ Created on Thu Jun  4 00:13:50 2020
 from setuptools import setup
 
 setup(name='gym_seb',
-      version='0.0.4',
+      version='0.0.5',
       description='gym for sebastian',
       #packages=setuptools.find_packages(include="gym_seb*"),
       install_requires=['gym>=0.20.0', 'pybullet', 'numpy', 'pandas']  # And any other dependencies foo needs
