05196824b8c5d14a4ebbf5347eeacf0d4457bdd3
ahhuang007 <ahhuang007@gmail.com>
2022-02-21 17:05:45 -0500

    updated plotter a bit
    updated plotter to show r_reward plots

--- plotter.py
+++ plotter.py
@@ -11,11 +11,11 @@ import numpy as np
 from scipy.ndimage.filters import uniform_filter1d
 
 #Loading data, set N for moving average
-df = pd.read_csv("./data/timestep_rewards/ppo_rewards_v18.csv")
+df = pd.read_csv("./data/timestep_rewards/ppo_rewards_v19.csv")
 #df = pd.read_csv("./../../../Downloads/ppo_rewards_v14.csv")
 N = 10000
 
-cols = ['f_reward', 'd_reward', 'p_reward', 'y_reward', 'c_reward']
+cols = ['f_reward', 'd_reward', 'p_reward', 'y_reward', 'c_reward', 'r_reward']
 for col in cols:
     #Removing outliers
     avg = np.mean(df[col])
