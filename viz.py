926d85524d39c924af116322156dcc7df91eee43
ahhuang007 <ahhuang007@gmail.com>
2022-06-26 03:02:21 -0400

    updated leg ranges in env
    forgot to update env, hopefully should be better now

--- viz.py
+++ viz.py
@@ -20,12 +20,12 @@ import gym_seb
 from stable_baselines3.common.evaluation import evaluate_policy
 import random
 
-env = gym.make('seb-v0', episode_timesteps = 10000, use_gui = False)
+env = gym.make('seb-v0', episode_timesteps = 10000, use_gui = True)
 
 
 mode = p.POSITION_CONTROL
 
-model = PPO.load("models/real_model_ppo_v20", env = env)
+model = PPO.load("models/real_model_ppo_v21", env = env)
 env.seed(4)
 model.set_random_seed(4)
 env.action_space.seed(4)
@@ -45,7 +45,7 @@ for j in range(1):
         obs, rewards, done, info = env.step(action)
         actions.append([action[2], action[10], action[11]])
         reward += rewards
-        #time.sleep(1/240)
+        time.sleep(1/240)
         #print(obs)
         #i += 1
     testing_rewards.append(reward)
